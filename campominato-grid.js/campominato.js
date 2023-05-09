feb3767604bfea1549246324c98986ce2dafeaf5
Zakaria96 <zakarialavoro@gmail.com>
2023-05-09 16:13:30 +0200

    esercizio base completato

--- campominato-grid.js/campominato.js
+++ campominato-grid.js/campominato.js
@@ -5,21 +5,24 @@ Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro e
 
 // variabili globali 
 const containerCelle = document.getElementById("main-container")
-let celle = " ";
-console.log(celle);
 
 // L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
 
 
-for (let i = 1 ; i <= 100 ; i++){ // Ogni cella ha un numero progressivo, da 1 a 100.
-    let classe = "";
-   
-        celle = i + " ";
-        // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-    containerCelle.innerHTML += `<div class="square">${celle}</div>`;
+for (let i = 1 ; i <= 100 ; i++){ // Ogni cella ha un numero progressivo, da 1 a 100.ù
+    let cella = document.createElement("div")
+    cella.className = 'cella';
+    cella.innerText = i;
+    containerCelle.appendChild(cella);
+
+    // Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+    cella.addEventListener('click',
+    function () {
+        cella.classList.add("bg-cell");
+        console.log(cella.innerText);
+    }
+    )
 }
-
-
 
 // FUNZIONI 
 
